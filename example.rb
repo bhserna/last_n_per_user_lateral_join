b8680b46489d783074e4eb75234c12332506c310
Benito Serna <bhserna@gmail.com>
2024-07-31 10:14:35 -0600

    update comments

--- example.rb
+++ example.rb
@@ -117,86 +117,86 @@ example "In a has many association" do
     }
   end
 
-  users = User.preload(:last_posts).limit(5)
+  users = User.preload(:last_posts).limit(10)
   pp users.map { |user| [user.name, user.last_posts.map(&:id)] }
 end
 
 # Setup
 # -----
 # -- create_table(:users)
-#    -> 0.0262s
+#    -> 0.0264s
 # -- create_table(:posts)
-#    -> 0.0031s
+#    -> 0.0026s
 # -- add_index(:posts, :user_id)
-#    -> 0.0016s
+#    -> 0.0011s
 # 
 # 
 # Example: SQL query
 # ------------------
-# D, [2022-08-16T06:26:29.015001 #14740] DEBUG -- :    (1.1ms)      SELECT selected_posts.* FROM users JOIN LATERAL (
+# D, [2024-07-31T10:13:37.939882 #12364] DEBUG -- :    (1.0ms)      SELECT selected_posts.* FROM users JOIN LATERAL (
 #       SELECT * FROM posts
 #       WHERE user_id = users.id
 #       ORDER BY id DESC LIMIT 3
 #     ) AS selected_posts ON TRUE
 # 
-# [{"id"=>100, "user_id"=>1, "title"=>"Grated Goats"}, {"id"=>99, "user_id"=>1, "title"=>"Nutty Dairy"}, {"id"=>98, "user_id"=>1, "title"=>"Grated Brie"}, {"id"=>200, "user_id"=>2, "title"=>"Smokey Goats"}, {"id"=>199, "user_id"=>2, "title"=>"Sharp Dairy"}, {"id"=>198, "user_id"=>2, "title"=>"Melting Brie"}, {"id"=>300, "user_id"=>3, "title"=>"Melting Sheep"}, {"id"=>299, "user_id"=>3, "title"=>"Soft Affineurs"}, {"id"=>298, "user_id"=>3, "title"=>"Nutty Coulommiers"}, {"id"=>400, "user_id"=>4, "title"=>"Dutch Gouda"}, {"id"=>399, "user_id"=>4, "title"=>"Smokey Gouda"}, {"id"=>398, "user_id"=>4, "title"=>"Sharp Affineurs"}, {"id"=>500, "user_id"=>5, "title"=>"Soft Dairy"}, {"id"=>499, "user_id"=>5, "title"=>"Fat Coulommiers"}, {"id"=>498, "user_id"=>5, "title"=>"Melting Alpine"}, {"id"=>600, "user_id"=>6, "title"=>"Milky Gouda"}, {"id"=>599, "user_id"=>6, "title"=>"Soft Goats"}, {"id"=>598, "user_id"=>6, "title"=>"Smokey Brie"}, {"id"=>700, "user_id"=>7, "title"=>"Melting Brie"}, {"id"=>699, "user_id"=>7, "title"=>"Soft Goats"}, {"id"=>698, "user_id"=>7, "title"=>"Sharp Goats"}, {"id"=>800, "user_id"=>8, "title"=>"Grated Dairy"}, {"id"=>799, "user_id"=>8, "title"=>"Milky Brie"}, {"id"=>798, "user_id"=>8, "title"=>"Sharp Gouda"}, {"id"=>900, "user_id"=>9, "title"=>"Grated Gouda"}, {"id"=>899, "user_id"=>9, "title"=>"Dutch Goats"}, {"id"=>898, "user_id"=>9, "title"=>"Grated Goats"}, {"id"=>1000, "user_id"=>10, "title"=>"Fat Coulommiers"}, {"id"=>999, "user_id"=>10, "title"=>"Soft Affineurs"}, {"id"=>998, "user_id"=>10, "title"=>"Dutch Alpine"}]
+# [{"id"=>100, "user_id"=>1, "title"=>"Cheesed Alpine"}, {"id"=>99, "user_id"=>1, "title"=>"Nutty Cows"}, {"id"=>98, "user_id"=>1, "title"=>"Soft Brie"}, {"id"=>200, "user_id"=>2, "title"=>"Grated Cows"}, {"id"=>199, "user_id"=>2, "title"=>"Melting Goats"}, {"id"=>198, "user_id"=>2, "title"=>"Melting Coulommiers"}, {"id"=>300, "user_id"=>3, "title"=>"Grated Brie"}, {"id"=>299, "user_id"=>3, "title"=>"Grated Brie"}, {"id"=>298, "user_id"=>3, "title"=>"Grated Coulommiers"}, {"id"=>400, "user_id"=>4, "title"=>"Cheesed Affineurs"}, {"id"=>399, "user_id"=>4, "title"=>"Dutch Dairy"}, {"id"=>398, "user_id"=>4, "title"=>"Soft Goats"}, {"id"=>500, "user_id"=>5, "title"=>"Cheeky Sheep"}, {"id"=>499, "user_id"=>5, "title"=>"Sharp Cows"}, {"id"=>498, "user_id"=>5, "title"=>"Smokey Goats"}, {"id"=>600, "user_id"=>6, "title"=>"Cheeky Gouda"}, {"id"=>599, "user_id"=>6, "title"=>"Dutch Affineurs"}, {"id"=>598, "user_id"=>6, "title"=>"Melting Alpine"}, {"id"=>700, "user_id"=>7, "title"=>"Cheeky Coulommiers"}, {"id"=>699, "user_id"=>7, "title"=>"Smokey Sheep"}, {"id"=>698, "user_id"=>7, "title"=>"Melting Coulommiers"}, {"id"=>800, "user_id"=>8, "title"=>"Milky Coulommiers"}, {"id"=>799, "user_id"=>8, "title"=>"Grated Goats"}, {"id"=>798, "user_id"=>8, "title"=>"Sharp Goats"}, {"id"=>900, "user_id"=>9, "title"=>"Cheeky Cows"}, {"id"=>899, "user_id"=>9, "title"=>"Dutch Sheep"}, {"id"=>898, "user_id"=>9, "title"=>"Cheeky Brie"}, {"id"=>1000, "user_id"=>10, "title"=>"Dutch Gouda"}, {"id"=>999, "user_id"=>10, "title"=>"Melting Dairy"}, {"id"=>998, "user_id"=>10, "title"=>"Cheeky Sheep"}]
 # 
 # 
 # Example: Lateral join with rails with a fixed n
 # -----------------------------------------------
-# D, [2022-08-16T06:26:29.018260 #14740] DEBUG -- :   Post Load (0.6ms)  SELECT "posts".* FROM (SELECT selected_posts.* FROM "users" JOIN LATERAL (
+# D, [2024-07-31T10:13:37.942006 #12364] DEBUG -- :   Post Load (0.4ms)  SELECT "posts".* FROM (SELECT selected_posts.* FROM "users" JOIN LATERAL (
 #           SELECT * FROM posts
 #           WHERE user_id = users.id
 #           ORDER BY id DESC LIMIT 3
 #         ) AS selected_posts ON TRUE) posts
-# D, [2022-08-16T06:26:29.031011 #14740] DEBUG -- :   User Load (0.4ms)  SELECT "users".* FROM "users" WHERE "users"."id" IN ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)  [["id", 1], ["id", 2], ["id", 3], ["id", 4], ["id", 5], ["id", 6], ["id", 7], ["id", 8], ["id", 9], ["id", 10]]
-# [["Buddy Mohr", [100, 99, 98]],
-#  ["Renea Abshire", [200, 199, 198]],
-#  ["Liliana Funk", [300, 299, 298]],
-#  ["Floretta Wiegand", [400, 399, 398]],
-#  ["Jerilyn Zulauf", [500, 499, 498]],
-#  ["Margaret Schowalter", [600, 599, 598]],
-#  ["Rosemary Wilkinson", [700, 699, 698]],
-#  ["Brian Emmerich", [800, 799, 798]],
-#  ["Mammie Leuschke", [900, 899, 898]],
-#  ["Theodora Howe", [1000, 999, 998]]]
+# D, [2024-07-31T10:13:37.948037 #12364] DEBUG -- :   User Load (0.3ms)  SELECT "users".* FROM "users" WHERE "users"."id" IN ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)  [["id", 1], ["id", 2], ["id", 3], ["id", 4], ["id", 5], ["id", 6], ["id", 7], ["id", 8], ["id", 9], ["id", 10]]
+# [["Leida Doyle", [100, 99, 98]],
+#  ["Domitila Rowe", [200, 199, 198]],
+#  ["Elsie Swift", [300, 299, 298]],
+#  ["Minna Macejkovic", [400, 399, 398]],
+#  ["Joannie Friesen", [500, 499, 498]],
+#  ["Milagro Greenfelder", [600, 599, 598]],
+#  ["Sylvie Harris", [700, 699, 698]],
+#  ["Paulina Funk", [800, 799, 798]],
+#  ["Anita Towne", [900, 899, 898]],
+#  ["Sanjuanita Blick", [1000, 999, 998]]]
 # 
 # 
 # Example: Lateral join with rails with a variable n
 # --------------------------------------------------
-# D, [2022-08-16T06:26:29.038735 #14740] DEBUG -- :   Post Load (1.2ms)  SELECT "posts".* FROM (SELECT selected_posts.* FROM "users" JOIN LATERAL (
+# D, [2024-07-31T10:13:37.951483 #12364] DEBUG -- :   Post Load (0.6ms)  SELECT "posts".* FROM (SELECT selected_posts.* FROM "users" JOIN LATERAL (
 #           SELECT * FROM posts
 #           WHERE user_id = users.id
 #           ORDER BY id DESC LIMIT 3
 #         ) AS selected_posts ON TRUE) posts
-# D, [2022-08-16T06:26:29.040289 #14740] DEBUG -- :   User Load (0.4ms)  SELECT "users".* FROM "users" WHERE "users"."id" IN ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)  [["id", 1], ["id", 2], ["id", 3], ["id", 4], ["id", 5], ["id", 6], ["id", 7], ["id", 8], ["id", 9], ["id", 10]]
-# [["Buddy Mohr", [100, 99, 98]],
-#  ["Renea Abshire", [200, 199, 198]],
-#  ["Liliana Funk", [300, 299, 298]],
-#  ["Floretta Wiegand", [400, 399, 398]],
-#  ["Jerilyn Zulauf", [500, 499, 498]],
-#  ["Margaret Schowalter", [600, 599, 598]],
-#  ["Rosemary Wilkinson", [700, 699, 698]],
-#  ["Brian Emmerich", [800, 799, 798]],
-#  ["Mammie Leuschke", [900, 899, 898]],
-#  ["Theodora Howe", [1000, 999, 998]]]
+# D, [2024-07-31T10:13:37.951946 #12364] DEBUG -- :   User Load (0.2ms)  SELECT "users".* FROM "users" WHERE "users"."id" IN ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)  [["id", 1], ["id", 2], ["id", 3], ["id", 4], ["id", 5], ["id", 6], ["id", 7], ["id", 8], ["id", 9], ["id", 10]]
+# [["Leida Doyle", [100, 99, 98]],
+#  ["Domitila Rowe", [200, 199, 198]],
+#  ["Elsie Swift", [300, 299, 298]],
+#  ["Minna Macejkovic", [400, 399, 398]],
+#  ["Joannie Friesen", [500, 499, 498]],
+#  ["Milagro Greenfelder", [600, 599, 598]],
+#  ["Sylvie Harris", [700, 699, 698]],
+#  ["Paulina Funk", [800, 799, 798]],
+#  ["Anita Towne", [900, 899, 898]],
+#  ["Sanjuanita Blick", [1000, 999, 998]]]
 # 
 # 
 # Example: In a has many association
 # ----------------------------------
-# D, [2022-08-16T06:26:29.045333 #14740] DEBUG -- :   User Load (0.6ms)  SELECT "users".* FROM "users"
-# D, [2022-08-16T06:26:29.051624 #14740] DEBUG -- :   Post Load (0.8ms)  SELECT "posts".* FROM (SELECT selected_posts.* FROM "users" JOIN LATERAL (
+# D, [2024-07-31T10:13:37.952927 #12364] DEBUG -- :   User Load (0.1ms)  SELECT "users".* FROM "users" LIMIT $1  [["LIMIT", 10]]
+# D, [2024-07-31T10:13:37.955446 #12364] DEBUG -- :   Post Load (0.7ms)  SELECT "posts".* FROM (SELECT "users"."id" AS user_id, "selected_posts"."id", "selected_posts"."title" FROM "users" JOIN LATERAL (
 #           SELECT * FROM posts
 #           WHERE user_id = users.id
 #           ORDER BY id DESC LIMIT 3
 #         ) AS selected_posts ON TRUE) posts WHERE "posts"."user_id" IN ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)  [["user_id", 1], ["user_id", 2], ["user_id", 3], ["user_id", 4], ["user_id", 5], ["user_id", 6], ["user_id", 7], ["user_id", 8], ["user_id", 9], ["user_id", 10]]
-# [["Buddy Mohr", [100, 99, 98]],
-#  ["Renea Abshire", [200, 199, 198]],
-#  ["Liliana Funk", [300, 299, 298]],
-#  ["Floretta Wiegand", [400, 399, 398]],
-#  ["Jerilyn Zulauf", [500, 499, 498]],
-#  ["Margaret Schowalter", [600, 599, 598]],
-#  ["Rosemary Wilkinson", [700, 699, 698]],
-#  ["Brian Emmerich", [800, 799, 798]],
-#  ["Mammie Leuschke", [900, 899, 898]],
-#  ["Theodora Howe", [1000, 999, 998]]]
+# [["Leida Doyle", [100, 99, 98]],
+#  ["Domitila Rowe", [200, 199, 198]],
+#  ["Elsie Swift", [300, 299, 298]],
+#  ["Minna Macejkovic", [400, 399, 398]],
+#  ["Joannie Friesen", [500, 499, 498]],
+#  ["Milagro Greenfelder", [600, 599, 598]],
+#  ["Sylvie Harris", [700, 699, 698]],
+#  ["Paulina Funk", [800, 799, 798]],
+#  ["Anita Towne", [900, 899, 898]],
+#  ["Sanjuanita Blick", [1000, 999, 998]]]
